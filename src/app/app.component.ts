e0a5ad4add19a4a7681338038e455f5d5cca41a2
Lucas Kuess <lucas@kuess.io>
2022-05-03 13:54:44 +0200

    Basic Home Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, of, ReplaySubject, Subject } from 'rxjs';
+import { Todo } from './_models/todo';
+import { TodoService } from './_services/todo.service';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.Default,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Todo-List';
+  $todos = new Observable<Todo[]>();
+
+  constructor(private todoService: TodoService){
+
+  }
+  ngOnInit(): void {
+    this.todoService.getTodos();
+    this.$todos = this.todoService.$todos;
+  }
+  trackByDate(index:number,item:Todo){
+    return item.created
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TodoComponent } from './_components/todo/todo.component';
-
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CreateComponent } from './_components/create/create.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { SortByDatePipe } from './_pipes/sort-by-date.pipe';
+import { SortByStatePipe } from './_pipes/sort-by-state.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    CreateComponent,
+    SortByDatePipe,
+    SortByStatePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatListModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
