0262803b1504b208f08f9464b3b8d1f3e6f5b644
Lucas Kuess <lucas@kuess.io>
2022-05-04 10:33:31 +0200

    Added Changedetection Onpush

--- src/app/_components/todo/todo.component.ts
+++ src/app/_components/todo/todo.component.ts
@@ -1,25 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { Todo } from 'src/app/_models/todo';
 import { TodoService } from 'src/app/_services/todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoComponent implements OnInit {
 
-  constructor(private todoService: TodoService) { }
+  constructor(private todoService: TodoService,private cdr: ChangeDetectorRef) { }
   @Input()
   todo!: Todo;
   ngOnInit(): void {
   }
   changeState()
   {
-    this.todoService.change(this.todo);    
+    this.todoService.change(this.todo);
   }
   delete(){
     this.todoService.delete(this.todo);
+  }
+  get runChangeDetection() {
+    console.log('TodoComponent - Checking the view');
+    return true;
   }
 
 }
